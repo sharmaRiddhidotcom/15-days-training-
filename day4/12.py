cb762895e273dcbad748c9ac0c370bea7f65e7f9
sharmaRiddhidotcom <84970849+sharmaRiddhidotcom@users.noreply.github.com>
2021-06-09 13:44:44 +0530

    Add files via upload

---
+++ day4/12.py
@@ -0,0 +1,9 @@
+x=5
+y=7
+print('x>y=',x>y)
+print('x<y=',x<y)
+print('x==y=',x==y)
+print('x!=y=',x!=y)
+print('x>=y=',x>=y)
+print('x<=y=',x<=y)
+
---
+++ day4/15.py
@@ -0,0 +1,7 @@
+x = 10
+y = 20
+list1 = [10, 20, 30, 40, 50]
+print(x in list1)
+print(y in list1)
+print(y not in list1)
+
